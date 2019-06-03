d4fe79445ddcbc2d982060542409b5c384e34ecc
tkc1009 <takashi.hosoi@live.jp>
2019-06-03 21:54:56 +0900

    add rockPaperScissors

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -17,6 +17,21 @@
  *   rockPaperScissors(4); // => [['rock', 'rock', 'rock', 'rock'], etc...]
  */
 
-const rockPaperScissors = () => {
-  // Your code here
+const rockPaperScissors = (n) => {
+  const weapons = ['rock', 'paper', 'scissors'];
+  const hands = [];
+  const rounds = n || 3;
+  function recurse(roundsLeft, played) {
+    if(roundsLeft === 0) {
+      hands.push(played);
+      return;
+    }
+    for(let i = 0; i < weapons.length; i++) {
+      const current = weapons[i];
+      recurse(roundsLeft - 1, played.concat(current));
+    }
+  };
+  recurse(rounds, []);
+  return hands;
+
 };
