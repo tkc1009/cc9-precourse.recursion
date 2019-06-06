63e68f24a55c3c94d632cde6f62b3daefafdf844
tkc1009 <takashi.hosoi@live.jp>
2019-06-07 01:38:35 +0900

    add getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -10,6 +10,21 @@
   But we don't like easy! So we'll make you write your own.
 */
 
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  const results = [];
+  function recurse(collectionLeft) {
+    if(collectionLeft.length === 0){
+      return;
+    }
+    for(let i = 0; i < collectionLeft.length; i++) {
+      for(let j = 0; j < collectionLeft[i].className.split(" ").length; j++){
+        if(collectionLeft[i].className.split(" ")[j] === className){
+          results.push(collectionLeft[i]);
+        }
+      }
+      recurse(collectionLeft[i].children);
+    }
+  };
+  recurse(this.document.firstElementChild.children);
+  return results;
 };
