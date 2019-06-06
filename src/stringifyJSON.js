412b0138ddbf258a470930ecdf2ff5b78e30f706
tkc1009 <takashi.hosoi@live.jp>
2019-06-07 05:35:13 +0900

    add stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,6 +6,58 @@ What does the output for stringifyJSON look like? Play around with JSON.stringif
 
 */
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (target) => {
+  let result = [];
+  function recurse(value){
+    if(toString.apply(value).split(' ')[1].match('Undefined') || toString.apply(value).split(' ')[1].match('Function')){
+      if(result[result.length - 1] === ':'){
+        result.splice(result.lastIndexOf('"'));
+        result.splice(result.lastIndexOf('"'));  
+      }
+    } else if(toString.apply(value).split(' ')[1].match('Null')){
+      for(let s = 0; s < String(value).length; s++){
+        result.push(String(value)[s]);
+      }
+    } else if(toString.apply(value).split(' ')[1].match('Array')){
+      result.push('[');
+      if(value.length !== 0){
+        for(let i = 0; i < value.length; i++){
+          if(result[result.length - 1] !== '['){
+            result.push(',');
+          }
+          recurse(value[i]);
+        }
+      }
+      result.push(']');
+    } else if(toString.apply(value).split(' ')[1].match('Object')){
+      result.push('{');
+      if(Object.keys(value).length !== 0){
+        for(let i = 0; i < Object.keys(value).length; i++){
+          if(result[result.length - 1] !== '{'){
+            result.push(',');
+          }
+          result.push('"');
+          for(let s = 0; s < Object.keys(value)[i].length; s++){
+            result.push(Object.keys(value)[i][s]);
+          }
+          result.push('"');
+          result.push(':');
+          recurse(value[Object.keys(value)[i]]);
+        }
+      }
+      result.push('}');
+    } else if(toString.apply(value).split(' ')[1].match('String')){
+      result.push('"');
+      for(let s = 0; s < value.length; s++){
+        result.push(value[s]);
+      }
+      result.push('"');
+    } else {
+      for(let s = 0; s < value.toString().length; s++){
+        result.push(value.toString()[s]);
+      }
+    }
+  }
+  recurse(target);
+  return result.join('');
 };
